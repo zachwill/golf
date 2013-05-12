4df43fbfc5f288c7c7d70f206da88dd0f5b4ab75
Zach Williams <hey@zachwill.com>
2013-05-12 13:24:42 -0700

    Move JSON players data to separate directory

--- pga.py
+++ pga.py
@@ -105,7 +105,7 @@ def find_players():
 def scrape_player(player):
     """Scrape a PGA tour player using his player ID."""
     print player
-    url = "http://www.pgatour.com/data/players/{0}".format(player)
+    url = "http://www.pgatour.com/data/players/{0}/".format(player)
     text = req.get(url, headers=headers).text
     html = lh.fromstring(text)
     links = html.cssselect("td > a")
@@ -116,18 +116,19 @@ def scrape_player(player):
             print "\t {0}".format(endpoint)
             data = req.get(endpoint, headers=headers).text
             # Make sure we have a place to save the data
-            disk_path = "data/players/{0}".format(player)
+            disk_path = "players/{0}".format(player)
             if not os.path.exists(disk_path):
                 os.makedirs(disk_path)
             # Save the data to disk
-            file_name = "data/players/{0}/{1}".format(player, href)
+            file_name = "players/{0}/{1}".format(player, href)
             with open(file_name, "w") as f:
-                f.write(data)
+                f.write(data.encode('utf-8'))
 
 
 def main():
     for player in find_players():
-        scrape_player(player)
+        if int(player) >= 1842:
+            scrape_player(player)
 
 
 if __name__ == '__main__':
