1a2f79aadc2a5c7162b7b9d6faf0a6edcb7d6835
Zach Williams <hey@zachwill.com>
2013-05-12 09:39:54 -0700

    Add ability to scrape tournaments

--- pga.py
+++ pga.py
@@ -4,6 +4,7 @@
 Scrape the PGA tour's official data endpoint.
 """
 
+import os
 import lxml.html as lh
 import requests as req
 
@@ -14,8 +15,8 @@ headers = {
 }
 
 
-def scrape(year):
-    """Scrape PGA JSON data for a given year."""
+def scrape_stats(year):
+    """Scrape PGA JSON stats data for a given year."""
     print year
     url = "http://www.pgatour.com/data/r/stats/{0}/".format(year)
     text = req.get(url, headers=headers).text
@@ -32,6 +33,48 @@ def scrape(year):
                 f.write(data)
 
 
+def find_tourneys():
+    """Find PGA tournaments."""
+    endpoints = []
+    url = "http://www.pgatour.com/data/r/"
+    text = req.get(url, headers=headers).text
+    html = lh.fromstring(text)
+    links = html.cssselect("td > a")
+    for link in links:
+        href = link.attrib["href"]
+        if href.strip("/").isdigit():
+            tournament = href.strip("/")
+            endpoints.append(tournament)
+    return endpoints
+
+
+def scrape_tourney(tournament):
+    """Scrape a given tournament."""
+    print tournament
+    url = "http://www.pgatour.com/data/r/{0}/".format(tournament)
+    text = req.get(url, headers=headers).text
+    html = lh.fromstring(text)
+    links = html.cssselect("td > a")
+    for link in links:
+        href = link.attrib["href"]
+        if href.endswith("json"):
+            endpoint = url + href
+            print "\t {0}".format(endpoint)
+            data = req.get(endpoint, headers=headers).text
+            # Make sure we have a place to save the data
+            disk_path = "data/tournaments/{0}".format(tournament)
+            if not os.path.exists(disk_path):
+                os.makedirs(disk_path)
+            # Save the data to disk
+            file_name = "data/tournaments/{0}/{1}".format(tournament, href)
+            with open(file_name, "w") as f:
+                f.write(data)
+
+
+def main():
+    for tournament in find_tourneys():
+        scrape_tourney(tournament)
+
+
 if __name__ == '__main__':
-    for year in range(1980, 2012):
-        scrape(year)
+    main()
